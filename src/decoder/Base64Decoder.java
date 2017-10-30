4879c56164132bf1329a8d33d0d58ab871d3d5b3
DaveDLeague <dave.dunn@jointheleague.org>
2017-10-29 17:58:31 -0700

    Update Base64Decoder.java

--- src/decoder/Base64Decoder.java
+++ src/decoder/Base64Decoder.java
@@ -1,7 +1,7 @@
 package decoder;
 
 public class Base64Decoder {
-	final char[] base64Chars = {
+	static final char[] base64Chars = {
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
